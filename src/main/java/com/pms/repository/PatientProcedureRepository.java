cf4e35d81509b9710aa072878e0363e62afdd1e8
Maneesh Chaudhary <ManeeshC2@citiustech.com>
2022-02-17 19:46:06 +0530

    Updated Patient Procedure APIs

--- src/main/java/com/pms/repository/PatientProcedureRepository.java
+++ src/main/java/com/pms/repository/PatientProcedureRepository.java
@@ -13,6 +13,7 @@ import com.pms.entity.PatientProcedureEntity;
 public interface PatientProcedureRepository extends JpaRepository<PatientProcedureEntity, Long> {
 
 	@Query(value = " Select p.procedure_id , p.procedure_code , p.procedure_description , p.procedure_is_depricated "
+			+ " pp.appointment_id ,  pp.created_date , pp.patient_procedure_id , pp.procedure_details  "
 			+ " from pmsschema.patient_procedures as pp inner join  pmsschema.procedures as p "
 			+ " on pp.procedure_id=p.procedure_id where pp.patient_id=  "
 			+ "( SELECT patient_id FROM pmsschema.patientappointment as pa where pa.appointment_id = :appointmentId ) ", nativeQuery = true)
--- src/main/java/com/pms/service/PatientProcedureServiceImpl.java
+++ src/main/java/com/pms/service/PatientProcedureServiceImpl.java
@@ -55,5 +55,6 @@ public class PatientProcedureServiceImpl implements PatientProcedureService {
 		repository.deleteById(patientProcedureId);
 	}
 	
+	
 
 }
