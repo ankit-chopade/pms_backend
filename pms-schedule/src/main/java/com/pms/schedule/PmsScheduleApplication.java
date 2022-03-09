cd446b7f1c396d3c87fb6ab7edf0d521ba436376
Ankit Chopade <ankit.chopade@outlook.com>
2022-03-09 12:46:43 +0530

    eureka client added

--- pms-schedule/src/main/java/com/pms/schedule/PmsScheduleApplication.java
+++ pms-schedule/src/main/java/com/pms/schedule/PmsScheduleApplication.java
@@ -2,8 +2,10 @@ package com.pms.schedule;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class PmsScheduleApplication {
 
 	public static void main(String[] args) {
