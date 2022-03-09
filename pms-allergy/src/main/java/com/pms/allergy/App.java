98e12ac95f0110c2f5d9b3b1b48c4b8530fd80cb
Jayshree More <JayshreeM@citiustech.com>
2022-03-09 12:47:26 +0530

    add eureka server in pom and add application proerties

--- pms-allergy/src/main/java/com/pms/allergy/App.java
+++ pms-allergy/src/main/java/com/pms/allergy/App.java
@@ -1,9 +1,14 @@
 package com.pms.allergy;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 /**
  * Hello world!
  *
  */
+@EnableEurekaClient
+@SpringBootApplication
 public class App 
 {
     public static void main( String[] args )
