69065409b04aebb5f76e75758410b626e3511800
Harsh Singhania <HarshS3@citiustech.com>
2022-03-16 20:38:06 +0530

    Patient dashboard commit

--- pms-visit/src/main/java/com/pms/visit/service/VitalSignServiceImpl.java
+++ pms-visit/src/main/java/com/pms/visit/service/VitalSignServiceImpl.java
@@ -1,6 +1,7 @@
 package com.pms.visit.service;
 
 import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import com.pms.visit.constants.PmsVisitConstants;
 import com.pms.visit.converter.VitalSignConverter;
+import com.pms.visit.dto.DashboardVitalDto;
 import com.pms.visit.dto.VitalSignDto;
 import com.pms.visit.entity.VitalSignEntity;
 import com.pms.visit.repository.VitalSignRepository;
@@ -38,6 +40,28 @@ public class VitalSignServiceImpl implements VitalSignService {
 		entity.setActiveStatus(PmsVisitConstants.ACTIVE_STATUS);
 		entity = repository.save(entity);
 		return converter.toDto(entity);
+	}
+
+	@Override
+	public List<String> getBloodPressure(Long patientId) {
+		return repository.findBloodPressureByPatientId(patientId);
+	}
+
+	@Override
+	public List<String> getRespirationRate(Long patientId) {
+		return repository.findRespirationRateByPatientId(patientId);
+	}
+	
+	@Override
+	public DashboardVitalDto getLatestVitalSigns(Long patientId) {
+		 String obj = repository.findLatestVitalSignsByPatientId(patientId);
+		 String vitalSigns[] = obj.split(",");
+		 return new DashboardVitalDto(
+				 Integer.parseInt(vitalSigns[0]),
+				 vitalSigns[1],
+				 Integer.parseInt(vitalSigns[2]),
+				 vitalSigns[3]
+				 );
 	}
 
 }
