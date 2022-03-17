c429e12aec4f93877061f3a1d554ffdda17c5e04
Ankit Chopade <ankit.chopade@outlook.com>
2022-03-17 18:58:30 +0530

    Inbox added - Test

---
+++ pms-schedule/src/test/java/com/pms/schedule/service/AppointmentInboxServiceImplTest.java
@@ -0,0 +1,29 @@
+package com.pms.schedule.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AppointmentInboxServiceImplTest {
+
+	@Test
+	void testGetAppointmentsByDate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetAppointmentsByDateAndPatientId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetAppointmentsByDateAndPhysiciantId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDeleteAppointment() {
+		fail("Not yet implemented");
+	}
+
+}
